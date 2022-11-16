28399154229ae32fd9e541cecbd1a6dfde9f4ede
unknown <dmena@creditu.com>
2022-11-16 01:00:50 -0600

    Agregando rutas

--- api/index.js
+++ api/index.js
@@ -4,35 +4,26 @@ const axios = require('axios')
 
 const {API_KEY} = process.env
 
-
-// Syncing all the models at once.
 conn.sync({ 
   force: true 
 })
   .then(async () => {
 
     const apiMarvelCharacters = await axios.get(`https://gateway.marvel.com:443/v1/public/characters${API_KEY}`)
-    console.log(apiMarvelCharacters.data.data.results);
     let apiCharacters = apiMarvelCharacters.data.data.results
 
-    // console.log(apiCharacters);
-
     apiCharacters = apiCharacters?.map(character => {
-        console.log(character.comics)
       return {
         id: character.id,
         name: character.name,
         description: character.description,
         thumbnail: `${character.thumbnail.path}` + `.${character.thumbnail.extension}`,
-        // comics: [{"resourceURI": "http://gateway.marvel.com/v1/public/comics/21366",
-        // "name": "Avengers: The Initiative (2007) #14"}],
         comics: character.comics.items,
         series: character.series.items,
         stories: character.stories.items,
       }
     })
 
-//   console.log(apiCharacters)
   await Character.bulkCreate(apiCharacters)
 
   console.log('Base de datos conectada correctamente')
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,60 +1,15 @@
 const { Router } = require('express');
-// const axios = require('axios')
 const router = Router();
-const { Country, TouristACT } = require('../db.js');
-
-
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
+const { Character } = require('../db.js');
 
 
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-//axios.get('https://restcountries.eu/rest/v2/all')
+const getCharactersDBInfo =  () => {
 
-
-// const getApiInfo = async () => {
-
-//   const apiCountry = await axios.get('https://restcountries.eu/rest/v2/all')
-//   const apiInfo = await apiCountry.data.map((info) => {
-//     return {
-//         id: info.alpha3Code,
-//         name: info.name,
-//         capital: info.capital,
-//         continent: info.region,
-//         subregion: info.subregion,
-//         flag: info.flag,
-//         population: info.population,
-//         area: info.area,
-//     }
-//   })
-//   return apiInfo
-
-// }
-
-///////////// ME TRAE LOS PAISES DE LA BD ///////////////
-const getCountriesDBInfo =  () => {
-
-  return Country.findAll({
+  return Character.findAll({
     includes: {
-      model: Country,
-      attributes: ['id','name','capital','continent','subregion','flag','population','area'],
-      through: {
-        attributes: []
-      }
-    }
-  }) 
-
-}
-
-///////////// ME TRAE LAS ACTIVIDADES DE LA BD ///////////////
-const getActivitiesDBInfo = async () => {
-
-  return await TouristACT.findAll({
-    includes: {
-      model: TouristACT,
-      attributes: ['name', 'duration', 'difficulty', 'season'],
+      model: Character,
+      attributes: ['id','name','description','thumbnail','comics','series','stories'],
       through: {
         attributes: []
       }
@@ -62,11 +17,9 @@ const getActivitiesDBInfo = async () => {
   }) 
 }
 
-
-///////////// ORDENA LOS PAISES Z - A ///////////////
 const getDBInfoSortedZA = async () => {
 
-  const dbCountries = await getCountriesDBInfo()
+  const dbCharacters = await getCharactersDBInfo()
   dbCountries.sort((a, b) =>{
     if(a.name > b.name){
       return -1
@@ -78,15 +31,14 @@ const getDBInfoSortedZA = async () => {
 
   }) 
 
-  return dbCountries
+  return dbCharacters
 
 }
 
-///////////// ORDENA LOS PAISES A - Z ///////////////
 const getADBInfoSortedAZ = async () => {
 
-  const dbCountries = await getCountriesDBInfo()
-  dbCountries.sort((a, b) =>{
+  const dbCharacters = await getCharactersDBInfo()
+  dbCharacters.sort((a, b) =>{
     if(a.name > b.name){
       return 1
     }
@@ -97,190 +49,58 @@ const getADBInfoSortedAZ = async () => {
 
   })
 
-  return dbCountries
+  return dbCharacters
 
 }
 
-const getDBInfoSortedPopulationAZ = async () => {
-
-  const dbPopulationAZ = await getCountriesDBInfo()
-  dbPopulationAZ.sort((a, b) =>{
-    if(a.population > b.population){
-      return 1
-    }
-    if(b.population > a.population  ) {
-      return -1
-    }
-    return 0
-
-  })
-
-  return dbPopulationAZ
-
-}
-
-const getDBInfoSortedPopulationZA = async () => {
-
-  const dbPopulationZA = await getCountriesDBInfo()
-  dbPopulationZA.sort((a, b) =>{
-    if(a.population > b.population){
-      return -1
-    }
-    if(b.population > a.population  ) {
-      return 1
-    }
-    return 0
-
-  })
-
-  return dbPopulationZA
-
-}
-
-router.get('/countries/population/:type', async (req, res) => {
-
-  const { type } = req.params
-  if(type === 'desc'){
-    let countriesPopulation = await getDBInfoSortedPopulationZA()
-    res.status(200).send(countriesPopulation)
-  } else if( type === 'asc'){
-    let countriesPopulation = await getDBInfoSortedPopulationAZ()
-    res.status(200).send(countriesPopulation)
-  } else{
-    res.status(404).send('opcion inválida')
-  }
-  
-})
-
-router.get('/countries', async (req, res) => {
+router.get('/characters', async (req, res) => {
 
   const name = req.query.name
-  let countries = await getCountriesDBInfo()
+  let characters = await getCharactersDBInfo()
 
   if(name){
-    let countryName = await countries.filter(country =>
-     country.name.toLowerCase().includes(name.toLowerCase()))
+    let characterName = await characters.filter(character =>
+        character.name.toLowerCase().includes(name.toLowerCase()))
 
-     countryName.length ?
-     res.status(200).send(countryName) :
-     res.status(404).send('No existe ese país')
+    characterName.length ?
+     res.status(200).send(characterName) :
+     res.status(404).send('No existe ese personaje')
   } else {
-    res.status(200).send(countries)
+    res.status(200).send(characters)
   }
 })
 
 ///////////// RENDERIZA LOS PAISES Z -A Ó A - Z ///////////////
-router.get('/countries/:type', async (req, res) => {
+router.get('/characters/:type', async (req, res) => {
 
   const { type } = req.params
   if(type === 'desc'){
-    let countriesZA = await getDBInfoSortedZA()
-    res.status(200).send(countriesZA)
+    let charactersZA = await getDBInfoSortedZA()
+    res.status(200).send(charactersZA)
   } else if( type === 'asc'){
-    let countriesAZ = await getADBInfoSortedAZ()
-    res.status(200).send(countriesAZ)
+    let charactersAZ = await getADBInfoSortedAZ()
+    res.status(200).send(charactersAZ)
   } else{
     res.status(404).send('opcion inválida')
   }
   
 })
 
-router.get('/countries/:id', async (req, res) =>{
+router.get('/characters/:id', async (req, res) =>{
   
   const { id } = req.query
   
-  Country.findOne({
+  Character.findOne({
     where: {
       id: id.toUpperCase()
     }
   })
-  .then((country) => {
+  .then((character) => {
     
-    res.status(200).json  ( country )
+    res.status(200).json(character)
   })
   .catch((error) => res.status(404).send(error))
   
-})
-
-router.post('/activity', async (req, res) => {
-  
-  const { name, difficulty, duration, season, countries} = req.body
-  
-  try{
-    
-    let [act, created] = await TouristACT.findOrCreate({
-      where: {
-        name: name,
-        difficulty: difficulty,
-        duration: duration,
-        season: season,
-      }})
-      
-      await act.setCountries(countries)
-      return res.status(200).json(act)
-      
-    } catch (error) {
-      console.log(error)
-    }
-  })
-  
-router.get('/activities', async (req, res) => {
-  
-  const { name } = req.query
-  const act = await getActivitiesDBInfo(TouristACT)
-  
-  if(name){
-
-    TouristACT.findOne({
-      where: {
-        name: name
-      }
-    })
-    .then((act) => {
-      res.send( act )
-    })
-    .catch((error) => res.status(404).send(error))
-
-
-
-
-  } else {
-    res.status(200).json(act)
-    }
-})
-    
-router.get('/activities/:id', async (req, res) => {
-  
-  const { id } = req.params;
-  try{
-    const country = await Country.findByPk(id.toUpperCase())
-    const activities = await country.getTouristACTs();
-    
-    const result = activities.map(t => t.toJSON())
-    res.status(200).json(result)
-    
-  } catch(err){
-    console.log(err)
-  }
-  
-})
-    
-                                 
+})                       
                       
 module.exports = router;
-
-
-
-
-
-
-
-
-// const getAllCountries = async () => {
-
-//   const apiInfo = await getApiInfo()
-//   const dbInfo = await getDBInfo()
-//   const allInfo = apiInfo.concat(dbInfo)
-//   return allInfo
-
-// }
